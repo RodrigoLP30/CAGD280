63a5999bd68ebec2f283de9a470dc3e211de569b
RodrigoLP30 <ralopez-patino@mail.csuchico.edu>
2023-10-31 13:25:03 -0700

    move ball and update movement script

--- GitTest/Assets/Scripts/PlayerController.cs
+++ GitTest/Assets/Scripts/PlayerController.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // implement player Movement
+
+    public float speed;
+    private Vector3 movePos;
+
+    private void MovePlayer()
+    {
+
+    }
+   
+    private void jump()
+    {
+
+    }
 }
